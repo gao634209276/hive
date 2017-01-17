f1eacae08cad3071d043468fcd7f4ae53352721c
gaoyajun <634209276@qq.com>
2017-01-17 10:11:21 +0800

    tblproperties

---
+++ src/udf/json/GenericUDFJSONArray.java
@@ -0,0 +1,31 @@
+package udf.json;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
+
+public class GenericUDFJSONArray extends GenericUDF {
+	private transient ObjectInspectorConverters.Converter converter;
+
+
+	@Override
+	public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+		if (objectInspectors.length != 1) {
+			throw new UDFArgumentException(
+					"The function json_array(jsonArray");
+		}
+		return null;
+	}
+
+	@Override
+	public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+		return null;
+	}
+
+	@Override
+	public String getDisplayString(String[] strings) {
+		return null;
+	}
+}
